a66f6ff98ff7614435bb5d35ad14d6bb17351f4e
Bashudev Poudel <Bashudev Poudel>
2017-03-10 00:07:31 +0545

    upload to server

---
+++ code/config.js
@@ -0,0 +1,13 @@
+var config={
+	database:{
+		mode:'local', // can be local or remote
+		host:'localhost',
+		port:27017,
+		dbname:'card',
+		username:'',
+		password:''
+	}
+};
+
+
+module.exports = config;
